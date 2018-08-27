77843193ee436c1e96db358b97530b687532d992
Caryn Humphreys <c.humphreys@vehikl.com>
2018-08-27 14:26:04 -0400

    blowing up all the things

--- frontend/src/components/ClientLoginForm.vue
+++ frontend/src/components/ClientLoginForm.vue
@@ -50,7 +50,6 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang='scss'>
 .no-client-id {
   // This shit is bourbon (being loaded in webpack scss-loader vue.config.js)

de244f3a81062699991e70634918ef4970ca595a
Caryn <c.humphreys@vehikl.com>
2019-04-05 14:52:21 -0400

    polishing up some of the fine print screens to get things working

--- frontend/src/components/ClientLoginForm.vue
+++ frontend/src/components/ClientLoginForm.vue
@@ -1,32 +1,34 @@
 <template>
-  <div class="no-client-id">
-    <div v-if="true">TRUE?</div>
-    <p>
-      After entering your client id and authorizing access, copy the code out of the URL and paste it in to the
-      authorization token input.
-    </p>
-    <p>This will be automated when this can be put on a secure domain).</p>
+  <div class="wait">
+    <div class="content">
+      <div v-if="true">TRUE?</div>
+      <p>
+        After entering your client id and authorizing access, copy the code out of the URL and paste it in to the
+        authorization token input.
+      </p>
+      <p>This will be automated when this can be put on a secure domain).</p>
 
-    <div>
-      We need your Spotify Client ID:
-      <input v-model="clientId">
-      <a
-        :href="`https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${authScopes}&redirect_uri=http%3A%2F%2Flocalhost:8080%2Fclient-callback`"
-      >Authorize</a>
-    </div>
-    <div>
-      Secret:
-      <input v-model="secret">
-    </div>
+      <div>
+        We need your Spotify Client ID:
+        <input v-model="clientId">
+        <a
+          :href="`https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${authScopes}&redirect_uri=http%3A%2F%2Flocalhost:8080%2Fclient-callback`"
+        >Authorize</a>
+      </div>
+      <div>
+        Secret:
+        <input v-model="secret">
+      </div>
 
-    <h4>Where's my Client ID?</h4>
-    <p>
-      You will need to log in and create a spotify app to get a client id. Follow this link and create one:
-      <a
-        target
-        href="https://developer.spotify.com/dashboard/applications"
-      >Register an App</a>
-    </p>
+      <h4>Where's my Client ID?</h4>
+      <p>
+        You will need to log in and create a spotify app to get a client id. Follow this link and create one:
+        <a
+          target
+          href="https://developer.spotify.com/dashboard/applications"
+        >Register an App</a>
+      </p>
+    </div>
   </div>
 </template>
 
